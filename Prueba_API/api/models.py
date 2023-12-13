206e00353138d260fc1f1b0872f87b744bc7d048
Juliana Cristancho <cristanchojuli@gmail.com>
2023-12-13 14:10:23 -0500

    advance3

--- Prueba_API/api/models.py
+++ Prueba_API/api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Payment(models.Model):
     name=models.CharField(max_length=150)
     surname=models.CharField(max_length=50)
-    card_number=models.BigIntegerField()
-    card_cvv=models.IntegerField()
+    card_number=models.CharField(max_length=50)
+    card_cvv=models.IntegerField(null=True)
     total_value=models.BigIntegerField()
     extra_description=models.CharField(max_length=500, blank=True, null=True)
     comission_value=models.FloatField(blank=True, null=True)
--- Prueba_API/api/views.py
+++ Prueba_API/api/views.py
@@ -6,6 +6,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from .models import Payment
 import json
+import re
 
 # Create your views here.
 
@@ -18,13 +19,14 @@ class PaymentView(View):
     def post(self, request):
         jd = json.loads(request.body)
         datos={}
-        str_card_number=str(jd['card_number'])
+        #str_card_number=str(jd['card_number'])
         str_card_cvv=str(jd['card_cvv'])
         float_comission_value=float(jd['total_value'])
-        if(len(str_card_number) >= 16 ):
+        if(len(jd['card_number']) >= 16 ):
+            final_card_number=hide_card_number(jd['card_number'])
             if(len(str_card_cvv)== 3):
                 if(jd['total_value'] >=1000):
-                    Payment.objects.create(name=jd['name'], surname=jd['surname'], card_number=jd['card_number'], card_cvv=jd['card_cvv'],total_value=jd['total_value'],extra_description=jd['extra_description'], comission_value=float_comission_value*0.03)
+                    Payment.objects.create(name=jd['name'], surname=jd['surname'], card_number=final_card_number, total_value=jd['total_value'],extra_description=jd['extra_description'], comission_value=float_comission_value*0.03+((float_comission_value*0.03)*0.19)+(float_comission_value*0.015))
                     datos={'message':'Success'}
                 else:
                     datos={'message':'El valor mínimo a pagar es de $1000'}
@@ -33,3 +35,17 @@ class PaymentView(View):
         else:
             datos={'message':'El número de tarjeta debe contener al menos 16 dígitos'}
         return JsonResponse(datos)
+    
+
+def hide_card_number(chain):
+    # Utilizar una expresión regular para encontrar números de cualquier longitud
+    pattern = r'(\d*)(\d{4})'
+    
+    def replace(match):
+        # Reemplazar los dígitos anteriores a los últimos 4 con asteriscos y mantener los últimos 4 dígitos
+        return '*' * len(match.group(1)) + match.group(2)
+    
+    # Aplicar la expresión regular y la función de reemplazo a la cadena
+    resultado = re.sub(pattern, replace, chain)
+    
+    return resultado
